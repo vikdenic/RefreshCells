5a8e15695eb7439614dd20c77a19bc1760bc0c06
vikdenic <vik.denic@gmail.com>
2014-08-14 09:58:22 -0500

    Initial Commit

---
+++ RefreshCells/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RefreshCells
+//
+//  Created by Vik Denic on 8/14/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RefreshCells/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RefreshCells
+//
+//  Created by Vik Denic on 8/14/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
